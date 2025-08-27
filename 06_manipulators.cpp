a537f44142ea20e56c8de21256b36a2b296cfcee
KRITI MITTAL <erkriti.mittal@gmail.com>
2025-08-28 02:34:23 +0530

    Create 06_manipulators.cpp

---
+++ 06_manipulators.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <iomanip>
+ using namespace std;
+ int main(){
+    //manipulators in c++
+int a= 45, b= 699, c= 129000;
+cout<<"Value of a is:"<<setw(6)<<a<<endl;
+cout<<"Value of b is:"<<setw(6)<<b<<endl;
+cout<<"Value of c is:"<<setw(6)<<c<<endl;
+
+
+
+    return 0;
+ }
