5d6aec1e257d3dffef4d4a9f642f298c5e5f3fb5
Kriti-dev07 <erkriti.mittal@gmail.com>
2025-09-02 10:50:28 +0530

    loops

---
+++ 07_loops.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main (){
+     cout<<"***Table of 6***"<<endl;
+for(int i=1;i<11;i++)
+{
+   
+    cout<<i*6<<endl;    //for loop
+
+}
+int j=1;
+ cout<<"**Table of 7**"<<endl;
+while (j<11)
+{
+  
+   cout<<j*7<<endl;
+   j++; // while loop
+}
+
+int k=1;
+ cout<<"**Table of 8**"<<endl;
+do {
+  cout<<k*8<<endl; 
+  k++;    //do-while loop
+}
+while(k<11);
+
+
+
+
+    return 0;
+}
